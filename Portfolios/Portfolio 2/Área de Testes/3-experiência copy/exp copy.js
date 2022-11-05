43e38d425e6d322cdd5be428c1dd0f34d6585ced
JvVenna <joaocovinha12@gmail.com>
2022-11-05 18:11:28 -0300

    commit initial

---
+++ Portfolios/Portfolio 2/Área de Testes/3-experiência copy/exp copy.js
@@ -0,0 +1,12 @@
+var hardware = document.querySelector('.hardware')
+
+function hardware() {
+
+
+    hardware.style.display = 'flex';
+}
+
+function redes() {
+
+    redes.style.display = 'flex';
+}
